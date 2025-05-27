3bce4209636bb86e7be330c2dba0c5a2ae191447
parvati Awrul <parvati23@navgurukul.org>
2025-05-27 12:44:25 +0530

    added  comment cors

--- db/connect.js
+++ db/connect.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-        process.env.MONGO_URl,
-    //   "mongodb+srv://parul:parul@cluster0.frnwhyz.mongodb.net/restorant?retryWrites=true&w=majority&appName=Cluster0",
-    );
+        process.env.MONGO_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(" MongoDB connection failed:", error.message);
--- index.js
+++ index.js
@@ -83,11 +83,11 @@
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const connectDB = require("./db/connect");
 const MenuItem = require('./models/MenuItem');
 require('dotenv').config();
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
